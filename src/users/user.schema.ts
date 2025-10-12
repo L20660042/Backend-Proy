94f9866f5ae114c6208d3792858c45712ccb212c
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-12 01:06:48 -0600

    nuevo

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,13 +1,17 @@
 import { Schema, Document } from 'mongoose';
 
-// Interfaz para los datos del usuario
-export interface User extends Document {
-  email: string;
-  password: string;
-}
-
-// Esquema de Mongoose para el modelo de usuario
 export const UserSchema = new Schema({
-  email: { type: String, unique: true, required: true },
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
+  correo: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  userType: { type: String, required: true },
 });
+
+export interface User extends Document {
+  nombre: string;
+  apellido: string;
+  correo: string;
+  password: string;
+  userType: string;
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User, UserSchema } from './user.schema'; 
-import { MailerModule } from '../mailer/mailer.module';
+import { UsersController } from './users.controller';
+import { User, UserSchema } from './user.schema';  // Asegúrate de importar UserSchema correctamente
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),  
-    MailerModule,  
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),  // 'User' como nombre de entidad y el esquema UserSchema
   ],
-  providers: [UsersService], 
+  providers: [UsersService],
   controllers: [UsersController],
 })
 export class UsersModule {}
