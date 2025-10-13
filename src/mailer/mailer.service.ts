142fd380c9fb441d75b4f119a232c336d5f7fdcc
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-12 21:32:30 -0600

    Update mailer.service.ts

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -8,25 +8,32 @@ export class MailerService {
   constructor() {
     // Configuramos el transporter para usar Gmail
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: 'gmail',  // Usamos el servicio Gmail
       auth: {
-        user: 'Luistoarzola@gmail.com', 
-        pass: 'lqos kqda nejm psql', 
+        user: 'luistoarzola@gmail.com',
+        pass: 'afqx oilp saoi mneu', 
       },
+      tls: {
+        rejectUnauthorized: false,  // Permite conexiones seguras sin verificar el certificado (puede ser útil en entornos de desarrollo)
+      },
+      port: 465,  // Usar puerto 465 para conexiones seguras (SSL)
+      secure: true,  // Usar SSL
+      connectionTimeout: 10000,  // Tiempo de espera de conexión (10 segundos)
     });
   }
 
   // Método para enviar el código de verificación
   async sendVerificationCode(email: string, code: string): Promise<void> {
     const mailOptions = {
-      from: 'Luistoarzola@gmail.com',  // Correo desde el que se envía
+      from: 'luistoarzola@gmail.com',  // Correo desde el que se envía
       to: email,  // Correo del destinatario
       subject: 'Código de Verificación',
       text: `Tu código de verificación es: ${code}`,  // El cuerpo del correo
     };
 
     try {
-      await this.transporter.sendMail(mailOptions);  // Enviamos el correo
+      // Enviamos el correo usando el transporter configurado
+      await this.transporter.sendMail(mailOptions);  
       console.log('Correo enviado');
     } catch (error) {
       console.error('Error al enviar el correo:', error);
