9ae31b54dcaeaeff6e40399a5612efa499f418b8
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-12 21:48:51 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Permitir solicitudes desde el frontend de GitHub Pages
-  app.enableCors({
-    origin: 'https://l20660042.github.io/Frontendproyecto', // Sin barra final al final de la URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
-  });
+app.enableCors({
+  origin: 'https://l20660042.github.io',  // Asegúrate de que esta URL sea la correcta
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
 
   await app.listen(3000); // Asegúrate de que el backend esté escuchando en el puerto 3000
 }
