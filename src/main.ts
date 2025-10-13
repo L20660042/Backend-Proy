dfa943ba2e71971e926da9966fd49b240317e3c8
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-12 20:44:25 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Permitir solicitudes desde el frontend que está en localhost:5173
   app.enableCors({
-    origin: 'https://l20660042.github.io/Frontend-Proy/', // Este es el origen correcto de tu frontend
+    origin: 'https://l20660042.github.io/Frontendproyecto/', // Este es el origen correcto de tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
   });
