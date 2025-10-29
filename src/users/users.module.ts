adaf44ce2f20da01d75ee5c58e4e10df8c62bd01
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-28 19:40:56 -0600

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
-
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MailService } from '../mailer/mailer.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, MailService],
+  exports: [UsersService], 
 })
 export class UsersModule {}
