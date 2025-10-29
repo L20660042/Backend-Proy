f6117fe3338803d7b2fa24d6ed6b773163ab0bdd
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-28 20:22:47 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // Permitir solicitudes desde el frontend de GitHub Pages
   app.enableCors({
     origin: ['https://l20660042.github.io',
-    'http://localhost:3000'],
+    'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
