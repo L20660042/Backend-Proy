13df3d7b0e87035e035809364699bf47a28d4607
L20660042 <l20660042@matehuala.tecnm.mx>
2025-11-27 00:24:04 -0600

    Update create-user.dto.ts

--- src/users/DTO/create-user.dto.ts
+++ src/users/DTO/create-user.dto.ts
@@ -1,7 +1,28 @@
+import { IsString, IsEmail, IsEnum, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsEnum([
+    'administrador',
+    'jefe-departamento', 
+    'docente',
+    'tutor',
+    'coordinador-tutorias',
+    'control-escolar',
+    'subdireccion-academica',
+    'estudiante'
+  ])
   user_type: string;
 }
